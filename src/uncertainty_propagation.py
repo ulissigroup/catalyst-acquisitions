11f5ed31e6634f565aabdb2dd3be07232e114998
willieneis <willie.neiswanger@gmail.com>
2020-01-29 11:28:39 -0500

    Update uncertainty_propagation functions

--- src/uncertainty_propagation.py
+++ src/uncertainty_propagation.py
@@ -8,18 +8,40 @@ import numpy as np
 
 
 
-def get_distrib_over_min_sites(site_value_distrib_list): 
+def get_distrib_over_min_sites(site_value_distrib_list, distrib_type='params',
+                               sim_type='samples', n_samples=1e5): 
     """
     Return distribution over the minimum of site values.
 
     Args:
-       site_value_distrib_list: list of distributions over site values, each
-                                element corresponding to a dfferent site.
+        site_value_distrib_list: list of distributions over site values, each
+                                 element corresponding to a dfferent site.
+        distrib_type: format of distrib in distrib_list, could be 'params' or
+                      'samples'.
+        sim_type: type of simuation to do, could be 'samples' or 'analytic'
+        n_samples: number of samples to generate (for 'samples' sim_type)
+        return_type: format of return distrib, could be 'params' or 'samples'
     """
-    pass
+    if sim_type == 'samples':
+        
+        # compute samp_list (for each site)
+        if distrib_type == 'params':
+            samp_list = [np.random.normal(loc=d['loc'], scale=d['scale'],
+                                          size=(n_samples,))
+                         for d in site_value_distr_list]
+
+        # compute mins array
+        min_arr = np.min(np.array(samp_list), 0)
+
+        # return mins array or dict of params
+        if return_type == 'params':
+            return {'loc': np.mean(min_arr), 'scale': np.std(min_arr)}
+        else:
+           return min_arr
 
 
-def get_distrib_over_surface_value(min_site_distrib, optimal_min_site_value):
+def get_distrib_over_surface_value(min_site_distrib, optimal_min_site_value,
+                                   distrib_type='params'):
     """
     Return distribution over the value of a surface. This distribution depends
     on the distribution over the minimum value of sites on a surface and on the
@@ -28,8 +50,11 @@ def get_distrib_over_surface_value(min_site_distrib, optimal_min_site_value):
     Args:
         min_site_distrib: distribution over the minimum site value on surface.
         optimal_min_site_value: optimal minimum site value on surface
+        distrib_type: format of distrib for min_site_distrib, could be 'params' or
+                      'samples'.
     """
-    pass
+    if distribe_type == 'params':
+        pass
 
 
 def get_distrib_over_bulk_value(surface_value_distrib_list):
