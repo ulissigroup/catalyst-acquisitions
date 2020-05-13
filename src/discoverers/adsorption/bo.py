37bba6ecade35c96ca75062b8e96f5b003bbed9b
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-05-13 02:51:52 +0000

    Update BayesianLinearModel for BO

--- src/discoverers/adsorption/bo.py
+++ src/discoverers/adsorption/bo.py
@@ -146,10 +146,10 @@ class BayesianLinearModel:
 
     def train(self, docs, energies):
         '''Construct a feature representation and fit model.'''
-        docs_vector = self.get_docs_vector(docs)
+        docs_mat = self.get_docs_mat(docs)
         energies = np.array(energies)
         try:
-            self.model.fit(docs_vector, energies)
+            self.model.fit(docs_mat, energies)
         except Exception:
             pass
 
@@ -159,26 +159,29 @@ class BayesianLinearModel:
         ### TODO: confirm below is correct when Exception occurs
 
         try:
-            docs_vector = self.get_docs_vector(docs)
-            predictions, uncertainties = self.model.predict(docs_vector, return_std=True)
+            docs_mat = self.get_docs_mat(docs)
+            predictions, uncertainties = self.model.predict(docs_mat, return_std=True)
         except Exception:
             predictions = np.zeros(len(docs))
             uncertainties = np.ones(len(docs))
 
         return predictions, uncertainties
 
-    def get_docs_vector(self, docs):
+    def get_docs_mat(self, docs, fingerprints=True):
         '''Make a random feature representation.'''
-        docs_vector = []
-        for d in docs:
-            docs_vector.append(np.array(d['shift']))
-            #
-            #rep = d['miller'][:4]
-            #rep.append(d['shift'])
-            #docs_vector.append(np.array(rep))
+        if fingerprints:
+            docs_mat = docs
+        else:
+            docs_list = []
+            for d in docs:
+                docs_list.append(np.array(d['shift']))
+                #
+                #rep = d['miller'][:4]
+                #rep.append(d['shift'])
+                #docs_list.append(np.array(rep))
 
-        docs_vector = np.array(docs_vector).reshape(-1, 1)
-        return docs_vector
+            docs_mat = np.array(docs_list).reshape(-1, 1)
+        return docs_mat
 
 
 
